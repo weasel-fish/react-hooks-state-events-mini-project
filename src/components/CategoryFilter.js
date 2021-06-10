3a47ee0497cda8060cec7f8a1b5422d5e5e83340
github username <github email address>
2021-06-10 09:31:02 -0400

    Remove redundant(?) category state from CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React/*, {useState}*/ from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, filterCats, catState}) {
+  //const [selected, setSelected] = useState('')
+  
+  function handleClick(e) {
+    //setSelected(e.target.value)
+    filterCats(e.target.value)
+  }
+  
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map(cat => {
+        return <button className={(catState === cat) ? 'selected' : ''} onClick={handleClick} key={cat} value={cat}>{cat}</button>
+      })}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Task() {
+function Task({text, category, removeTask}) {
+  
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={() => removeTask(text)} className="delete">X</button>
     </div>
   );
 }
